116949f3c740fbb0e53554678fb7d121c088c77d
hzp <hzhouping@outlook.com>
2023-09-22 17:11:32 +0800

    feat(router): 集成配置路由包

---
+++ packages/store/src/setupStore.ts
@@ -0,0 +1,16 @@
+import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+import type { App } from 'vue'
+
+export const store = createPinia()
+
+export function setupStore(app: App) {
+  store.use(
+    createPersistedState({
+      storage: localStorage,
+      key: (key) => key.toUpperCase()
+    })
+  )
+
+  app.use(store)
+}
--- packages/vite-config/src/common.ts
+++ packages/vite-config/src/common.ts
@@ -1,5 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import pages from 'vite-plugin-pages'
 import type { UserConfig } from 'vite'
 
 const commonConfig: UserConfig = {
@@ -10,7 +11,14 @@ const commonConfig: UserConfig = {
     reportCompressedSize: false,
     chunkSizeWarningLimit: 1024
   },
-  plugins: [vue(), vueJsx()]
+  plugins: [
+    vue(),
+    vueJsx(),
+    pages({
+      extensions: ['vue'],
+      exclude: ['**/components/**/*', '**/modules/**/*']
+    })
+  ]
 }
 
 export default commonConfig
